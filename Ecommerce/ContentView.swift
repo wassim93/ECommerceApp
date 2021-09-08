d5bfd645177ceabab1594ac44f6eafaa5305ffe1
wassim <b.wassim1993@hotmail.fr>
2021-09-09 00:32:39 +0100

    Initial Commit

---
+++ Ecommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ecommerce
+//
+//  Created by wassim on 9/9/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by wassim on 9/9/2021.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
